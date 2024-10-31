14cb5641347c354945404e4eddec1e3f2b7c790e
MalicknND <ndiayemalick062@gmail.com>
2024-10-31 12:49:04 +0100

    husky

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+require('dotenv').config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, async (req, res) => {
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
